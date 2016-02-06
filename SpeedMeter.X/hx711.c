77f5dc91da4feba95c9a8752cc115edae1853497
Alexey <darklexi494@mail.com>
2016-02-06 19:25:49 +0300

    Some fixes for not use float

--- SpeedMeter.X/hx711.c
+++ SpeedMeter.X/hx711.c
@@ -11,7 +11,7 @@ int init_hx711 ( void )
     
     //Receiving tare
     int i = 0;
-    uint64_t load_sum = 0;
+    uint32_t load_sum = 0;
     for ( i = 0; i < NUM_DATA_TARE; i++ )
     {
         load_sum += read_tenzo_data();
@@ -20,10 +20,9 @@ int init_hx711 ( void )
     return( 0 );
 }
 
-int32_t read_tenzo_data ( void )
+int32_t read_tenzo_data ( void ) // Max out is 2^24-1 as ADC is 24 bit
 {
-    uint32_t load = 0;
-    int32_t loadOut = 0;
+    uint32_t ADC_value = 0;
     
     while ( DAT_PIN );
     
@@ -31,26 +30,26 @@ int32_t read_tenzo_data ( void )
     for ( i = 0; i < 24; i++ )
     {
         CLK_PIN = 1;
-        load=load<<1;
+        ADC_value = ADC_value << 1;
         CLK_PIN = 0;
         if ( DAT_PIN )
-            load++;
+            ADC_value++;
     }
     
     CLK_PIN = 1;
-    load = load^0x800000;
-    loadOut = load/128.0f;
+    ADC_value = ADC_value ^ 0x800000;
+//    loadOut = load / 128.0f;
     CLK_PIN = 0;
     
-    return ( loadOut );
+    return ( ADC_value );
 }
 
 int32_t read_tared_tenzo_data ( void )
 {
-    return( read_tenzo_data() - tare_value );
+    return( (int32_t)read_tenzo_data() - (int32_t)tare_value );
 }
 
 int16_t read_calibrated_tenzo_data ( void )
 {
-    return( read_tared_tenzo_data()*HX711_MULTI/31472 );
+    return( read_tared_tenzo_data()*HX711_MULTI/31472/128 );
 }
