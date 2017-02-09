c009f50f18d000eec6545514a85ea8ce435675b5
KaiL4eK <darklexi494@gmail.com>
2017-02-09 23:32:23 +0300

    ++

---
+++ mpu_data_serial.py
@@ -0,0 +1,87 @@
+#! /usr/bin/python
+# coding: utf-8
+
+import serial
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+
+import common
+import nonblock as nb
+
+from datetime import datetime
+import csv
+
+serial_dspic 			= serial.Serial(port='/dev/ttyUSB0', baudrate=460800, timeout=15.0)
+serial_dspic.bytesize 	= serial.EIGHTBITS 		#number of bits per bytes
+serial_dspic.parity 	= serial.PARITY_NONE 	#set parity check: no parity
+serial_dspic.stopbits 	= serial.STOPBITS_ONE 	#number of stop bits
+
+accel_x 	= [0]
+accel_y 	= [0]
+accel_z 	= [0]
+
+gyro_x		= [0]
+gyro_y		= [0]
+gyro_z		= [0]
+
+sensor_time = 2./1000;
+
+t = nb.waitOnInput()
+
+def main():
+	print "Hello!"
+	
+	serial_dspic.write( '1' )
+	response = serial_dspic.read( 1 );
+
+	if response != '0':
+		exit()
+
+	print "Ok!"
+	fulltime = 0
+
+	t.start()
+
+	while nb.program_run:
+		if t.is_alive():
+
+			data = serial_dspic.read( 2 * 6 )
+
+			accel_x.append( common.bytes_2_int16( data[0:2] ) )
+			accel_y.append( common.bytes_2_int16( data[2:4] ) )
+			accel_z.append( common.bytes_2_int16( data[4:6] ) )
+
+			gyro_x.append( common.bytes_2_int16( data[6:8] ) )
+			gyro_y.append( common.bytes_2_int16( data[8:10] ) )
+			gyro_z.append( common.bytes_2_int16( data[10:12] ) )
+
+			fulltime += sensor_time
+			time_array.append(fulltime)
+
+		else:
+
+			serial_dspic.write( '1' )
+			serial_dspic.close()
+			break
+
+	out = np.array([accel_x, accel_y, accel_z, 
+					gyro_x, gyro_y, gyro_z])
+
+	csvfile = "{:%Y%d%m_%H_%M_%S}".format(datetime.now())
+	csvfile = "mpu_logs/" + csvfile + ".csv"
+
+	with open(csvfile, "w") as output:
+		writer = csv.writer(output, lineterminator='\n')
+		writer.writerows(out.T)
+
+	plt.plot( time_array, gyro_x,  'y-', label='Rate' )
+
+	plt.ylabel('Angle')
+	plt.xlabel('Time')
+	plt.grid()
+	plt.title('Serial')
+	plt.legend()
+	plt.show()
+
+main()
---
+++ plot_mpu_data.py
@@ -0,0 +1,47 @@
+import string
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import sys
+import csv
+
+filename = sys.argv[1]
+
+# Slider initialization
+axcolor = 'lightgoldenrodyellow'
+axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+
+alpha_slider = Slider(axfreq, 'alpha', 0.0, 1.0)
+
+def update(val):
+    alpha = alpha_slider.val
+
+    l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+
+    fig.canvas.draw_idle()
+
+alpha_slider.on_changed(update)
+
+
+
+# Main program
+with open(filename, 'rb') as csvfile:
+	reader = csv.reader(csvfile)
+	[ accel_x, accel_y, accel_z, 
+	  gyro_x, gyro_y, gyro_z ] = np.array( list(reader) ).T
+
+accel_angle_pitch   = atan2( accel_y, sqrt(accel_x*accel_x + accel_z*accel_z)) * RADIANS_TO_DEGREES;
+
+plt.plot( time_array, rate_yaw,  'y-', label='Rate' )
+plt.plot( time_array, angle_yaw,  'g-', label='Angle' )
+
+
+plt.ylabel('Angle')
+plt.xlabel('Time')
+plt.grid()
+plt.title('Yaw')
+plt.legend()
+
+plt.show()
